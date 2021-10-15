8b56bf9284be42223f954f999885f76d359127b0
andreyDanilenko <danilenko.a.g@mail.ru>
2021-10-15 22:16:13 +0300

    registration processing

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -9,6 +9,6 @@ export const registration = async (email, password) => {
         alert(response.data.message)
 
     } catch (e) {
-        alert(e)
+        alert(e.response.data.message)
     }
 }
