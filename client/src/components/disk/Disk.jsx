a73ecdb1e48ab6ef92e334e7ac1015b720ac9ca2
andreyDanilenko <danilenko.a.g@mail.ru>
2021-11-21 19:24:55 +0300

    setting up folder navigation

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -3,22 +3,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getFiles } from '../../actions/file'
 import FileList from './fileList/FileList';
 import './disk.scss'
+import Popup from './Popup';
+import { setPopupDisplay } from '../../reducers/fileReducer';
 
 function Disk() {
     const dispatch = useDispatch();
     const currentDir = useSelector(state => state.files.currentDir);
 
     useEffect(() => {
-        dispatch(getFiles())
-    }, [currentDir])
+        dispatch(getFiles(currentDir))
+    }, [currentDir]);
+
+    function showPopupHundler() {
+        dispatch(setPopupDisplay('flex'))
+    }
 
     return (
         <div className="disk">
             <div className="disk__btns">
                 <button className='disk__back'>Назад</button>
-                <button className='disk__create'>Создать папку</button>
+                <button className='disk__create' onClick={() => showPopupHundler()}>Создать папку</button>
             </div>
             <FileList />
+            <Popup />
         </div>
     )
 }
