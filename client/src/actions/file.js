4bfa12c8de9bedcdc9ccc5f7aee4c242ff0a20f3
andreyDanilenko <danilenko.a.g@mail.ru>
2021-11-21 19:22:24 +0300

    file creation action

--- client/src/actions/file.js
+++ client/src/actions/file.js
@@ -1,15 +1,30 @@
 import axios from "axios";
-import { setFiles } from '../reducers/fileReducer'
+import { addFile, setFiles } from '../reducers/fileReducer'
 
 export function getFiles(dirId) {
     return async dispatch => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/files/${dirId ? '?parent=' + dirId : ''}`, {
+            const response = await axios.get(`http://localhost:5000/api/files${dirId ? '?parent=' + dirId : ''}`, {
                 headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
             });
             dispatch(setFiles(response.data));
-            // dispatch(setFiles(response.data))
-            console.log(response.data);
+        } catch (e) {
+            alert(e.response.data.message);
+        }
+    }
+}
+
+export function createDir(dirId, name) {
+    return async dispatch => {
+        try {
+            const response = await axios.post('http://localhost:5000/api/files/', {
+                name,
+                parent: dirId,
+                type: 'dir'
+            }, {
+                headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+            });
+            dispatch(addFile(response.data));
         } catch (e) {
             alert(e.response.data.message);
         }
