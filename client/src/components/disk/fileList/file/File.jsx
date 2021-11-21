03fd19d0b6945380bc46badd2046668c792ed91e
andreyDanilenko <danilenko.a.g@mail.ru>
2021-11-21 19:24:37 +0300

    setting up folder navigation

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './file.scss';
 import dirLogo from '../../../../assets/img/folder.svg';
 import fileLogo from '../../../../assets/img/file.svg';
+import { setCurrentDir } from '../../../../reducers/fileReducer';
 
 function File({ file }) {
+    const dispatch = useDispatch()
+    const currentDir = useSelector(state => state.files.currentDir)
+
+    function openDirHundler() {
+        dispatch(setCurrentDir(file._id))
+    }
+
     return (
-        <div className="file">
+        <div className="file" onClick={file.type === 'dir' ? () => openDirHundler() : ''}>
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img" />
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date.slice(0, 10)}</div>
