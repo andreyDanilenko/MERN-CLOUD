ed4824e743b2d4efdafbc882cbb8b6afb2d9b327
andreyDanilenko <danilenko.a.g@mail.ru>
2021-11-22 03:06:43 +0300

    drag and drop file

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getFiles, uploadFile } from '../../actions/file'
 import FileList from './fileList/FileList';
 import './disk.scss'
 import Popup from './Popup';
-import { setPopupDisplay, setCurrentDir, addFile } from '../../reducers/fileReducer';
+import { setPopupDisplay, setCurrentDir } from '../../reducers/fileReducer';
 
 function Disk() {
     const dispatch = useDispatch();
     const currentDir = useSelector(state => state.files.currentDir);
     const dirStack = useSelector(state => state.files.dirStack);
+    const [dragEnter, setDragEnter] = useState(false);
+
 
     useEffect(() => {
         dispatch(getFiles(currentDir));
@@ -29,8 +31,28 @@ function Disk() {
         files.forEach(file => dispatch(uploadFile(file, currentDir)));
     };
 
-    return (
-        <div className="disk">
+    function dragEnterHundler(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        setDragEnter(true);
+    };
+
+    function dragLeaveHundler(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        setDragEnter(false);
+    };
+
+    function onDropHundler(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+        const files = [...evt.dataTransfer.files];
+        files.forEach(file => dispatch(uploadFile(file, currentDir)));
+        setDragEnter(false)
+    };
+
+    return (!dragEnter ?
+        <div className="disk" onDragEnter={dragEnterHundler} onDragLeave={dragLeaveHundler} onDragOver={dragEnterHundler}>
             <div className="disk__btns">
                 <button className='disk__back' onClick={() => backClickHundler()}>Назад</button>
                 <button className='disk__create' onClick={() => showPopupHundler()}>Создать папку</button>
@@ -42,6 +64,10 @@ function Disk() {
 
             <FileList />
             <Popup />
+        </div>
+        :
+        <div className="drag-area" onDrop={onDropHundler} onDragEnter={dragEnterHundler} onDragLeave={dragLeaveHundler} onDragOver={dragEnterHundler}>
+            Перетащите файлы сюда
         </div>
     )
 }
